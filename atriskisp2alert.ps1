fc2a5471d23a0405b81ca8cfa15684a29b2e8816
Stefán Þór Hreinsson <stefanhr@nyherji.is>
2017-03-07 20:42:49 +0000

    Add files via upload

---
+++ atriskisp2alert.ps1
@@ -0,0 +1,48 @@
+﻿<#  Short PowerShell Script to get ATRISK nodes                                    
+    Note:  Operates on a single server environment #>
+
+
+add-type @"
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    
+    public class IDontCarePolicy : ICertificatePolicy {
+        public IDontCarePolicy() {}
+        public bool CheckValidationResult(
+            ServicePoint sPoint, X509Certificate cert,
+            WebRequest wRequest, int certProb) {
+            return true;
+        }
+    }
+"@
+[System.Net.ServicePointManager]::CertificatePolicy = new-object IDontCarePolicy 
+
+$admin = "view"
+$secPw = ConvertTo-SecureString "iiWiphahjiezu6f" -AsPlainText -Force
+$cred = New-Object PSCredential -ArgumentList $admin,$secPw
+$headers = @{}
+$headers.Add( "OC-API-Version", "1.0" )
+$headers.Add( "Accept" , "application/json" )
+
+<# Call the Rest API to get the list of current clients
+   Note that if you have an unsigned certificate, you must add code to ignore it #>
+
+$nodelist = Invoke-RestMethod -uri "https://10.220.1.5:11090/oc/api/clients" -Credential $cred -Headers $headers -Method GET
+
+$nodelist.clients
+
+<#
+$riskcheck = $nodelist.clients
+
+<# Check it the node is atrisk #>
+<# $restcommand = "https://10.220.1.5:11090/oc/api/servers/"+ ($riskcheck.SERVER) +"/clients/"+ $riskcheck.Name +"/atrisk"
+$risk
+$restcommand
+$atrisk = Invoke-RestMethod -uri $restcommand -Credential $cred -Headers $headers -Method GET
+$atrisk
+}
+
+<# Build the command to lock the node and pass it to the rest API#>
+# $restcommand = "https://localhost:11090/oc/api/servers/" + ($locknode.SERVER) + "/clients/" + $locknode.Name + "/lock"
+# (Invoke-RestMethod -uri $restcommand -Credential $cred -Headers $headers -Method Put).Message.Messagefordev
+
---
+++ ispatrisk2alert.py
@@ -0,0 +1,38 @@
+﻿# Python 3.4
+#  'OC-API-Version: 1.0'
+#       servers/{server name}/clients/{client name}/atrisk
+# Script to send THJODSKAJ - ATRISK - NODEDNAME email to montoring admins
+
+import ssl
+import http.client
+import json
+import base64
+
+c = http.client.HTTPSConnection("localhost", 11090, context=ssl._create_unverified_context())
+credentials = base64.b64encode(b"view:iiWiphahjiezu6f").decode("ascii")
+
+headers = {"Authorization" : "Basic " + credentials, 'Accept': 'application/json', "OC-API-Version": "1.0"}
+c.request("GET", "/oc/api/clients",  headers=headers)
+r = c.getresponse()
+
+j = json.loads(r.read().decode('utf-8'))
+
+# Dump the result to STDOUT
+#print (j)
+
+#print(type(j))
+
+#for client in j['clients']:
+#    print(client['NAME'], client['DOMAIN'])
+
+#for client in j['clients']:
+#   print("hello " + client['NAME'])
+
+for client in j['clients']:
+    if client['SERVER'] == 'TSMNEW':
+       path = ("/oc/api/servers/TSMNEW/clients/" + client['NAME'] + "/atrisk")
+       print (path)
+       c.request("GET",path, headers=headers)
+       risk = c.getresponse()
+       atrisk = json.loads(risk.read().decode('utf-8'))
+       print (atrisk)
